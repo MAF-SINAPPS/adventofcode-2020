94d18afc583e8d62527b8953a65d41bf1525c660
MAF-SINAPPS <75315054+MAF-SINAPPS@users.noreply.github.com>
2020-12-03 20:45:13 +0100

    Algos Day1
    Algo Day 1 C# et F#

---
+++ MGO/Day1/C#/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Day1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = File.ReadAllLines("input.txt");
+            var values = input.Select(i=>int.Parse(i)).ToArray();
+
+            for(int i = 0; i < values.Length - 3; i++){
+                for(int j = i+1; j < values.Length - 2; j++){
+                    if(values[i] + values[j] == 2020){
+                        Console.WriteLine($"x={values[i]},y={values[j]}, x*y={ values[i]*values[j]}");
+                    }
+                    for(int k = j+1; k < values.Length - 1; k++){
+                        if(values[i] + values[j] + values[k] == 2020){
+                            Console.WriteLine($"x={values[i]},y={values[j]},z={values[k]}, x*y*z={ values[i]*values[j]*values[k]}");
+                        }
+                    }
+                }
+            }            
+        }
+    }
+}
---
+++ MGO/Day1/F#/Program.fs
@@ -0,0 +1,16 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+[<EntryPoint>]
+let main argv =
+    let values = System.IO.File.ReadAllLines("input.txt") |> Array.map int
+    for i in 0 .. values.Length - 3 do
+        for j in i+1 .. values.Length - 2 do
+            if(values.[i]+values.[j] = 2020) then
+                printfn "(%d, %d) => %d" values.[i] values.[j] (values.[i]*values.[j])
+            for k in j+1 .. values.Length - 1 do
+                if(values.[i]+values.[j]+values.[k] = 2020) then
+                    printfn "(%d, %d, %d) => %d" values.[i] values.[j] values.[k] (values.[i]*values.[j]*values.[k])
+
+    0 // return an integer exit code
