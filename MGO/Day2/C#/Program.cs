67857f2d7a1e02e22d1016a4f8baa386d4583c0e
MAF-SINAPPS <75315054+MAF-SINAPPS@users.noreply.github.com>
2020-12-03 20:50:07 +0100

    Algo Day 2

---
+++ MGO/Day2/C#/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Day2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = File.ReadAllLines("input.txt");            
+            var count=0;
+            Regex expression = new Regex(@"(?<min>\d+)-(?<max>\d+) (?<char>\w): (?<password>\w+)");
+            #region Part 1
+            // var values = input.Select(i=>{                
+            //     var match = expression.Match(i);
+            //     return new {
+            //         Min = int.Parse(match.Groups["min"].Value),
+            //         Max = int.Parse(match.Groups["max"].Value),
+            //         Char = match.Groups["char"].Value,
+            //         Password = match.Groups["password"].Value,
+            //     };
+            // });
+            // foreach(var o in values){
+            //     var nbChar = o.Password.Count(p=>p.ToString()==o.Char);
+            //     if(nbChar >= o.Min && nbChar <= o.Max)
+            //         count++;
+            // }
+            #endregion
+            #region Part 2
+            var values = input.Select(i=>{                
+                var match = expression.Match(i);
+                return new {
+                    Position1 = int.Parse(match.Groups["min"].Value),
+                    Position2 = int.Parse(match.Groups["max"].Value),
+                    Char = match.Groups["char"].Value,
+                    Password = match.Groups["password"].Value,
+                };
+            });
+            foreach(var o in values){
+                if(o.Password[o.Position1-1].ToString() == o.Char ^ o.Password[o.Position2-1].ToString() == o.Char)
+                    count++;
+            }
+            #endregion 
+            Console.WriteLine($"Nombre de mot de passe correct : {count}");
+        }
+    }
+}
