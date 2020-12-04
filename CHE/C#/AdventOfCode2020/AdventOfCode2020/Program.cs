47510b04cfb3948b5eb59b327f33a49e33ee5a8e
Clément Hervy <nemo.liquidlife@gmail.com>
2020-12-04 20:16:13 +0100

    Day 1-4 CHE

---
+++ CHE/C#/AdventOfCode2020/AdventOfCode2020/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace AdventOfCode2020.Puzzles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Day4Puzzle.Part1();
+            Day4Puzzle.Part2();
+        }
+    }
+}
