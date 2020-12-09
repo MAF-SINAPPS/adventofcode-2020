9e6519b323a26d4a286ee381365cd02815929ca6
ghazoui <hassane.ghazoui@maf.fr>
2020-12-09 10:25:33 +0100

    Part 2 - Day 9

--- HGH/2020/Day9/Program.cs
+++ HGH/2020/Day9/Program.cs
@@ -11,16 +11,37 @@ namespace AdventOfCode
         
         private static bool IsExist(double nbr, double[] last5Numbers)
         {
-            foreach (var item in last5Numbers)
+            for (int j = 0; j < last5Numbers.Length-1; j++)
             {
-                for (int i = 1; i < last5Numbers.Length; i++)
+                for (int i = j+1; i < last5Numbers.Length; i++)
                 {
-                    if (nbr == item + last5Numbers[i])
+                    if (nbr == last5Numbers[j] + last5Numbers[i])
                         return true;
                 }
             }
             return false;
         }
+
+        private static double GetSumOfSmallestAndLargest(double nbr, double[] last5Numbers)
+        {
+            List<double> listNbr = new List<double>();
+            double sum = 0;
+            for (int j = 0; j < last5Numbers.Length - 1 && sum!=nbr; j++)
+            {
+                sum = last5Numbers[j];
+                listNbr.RemoveAll(_=> _>=0);
+                listNbr.Add(last5Numbers[j]);
+                for (int i = j + 1; i < last5Numbers.Length && sum<=nbr; i++)
+                {
+                    sum += last5Numbers[i];
+                    listNbr.Add(last5Numbers[i]);
+                    if (nbr == sum)
+                        break;
+                }
+            }
+            return listNbr.Max() + listNbr.Min();
+        }
+
         static void Main(string[] args)
         {
             //------------------Day 9-------------------
@@ -32,6 +53,9 @@ namespace AdventOfCode
                 if (!IsExist(lines[i], lines.Skip(i - 25).Take(25).ToArray()))
                 {
                     result = lines[i];
+                    //Start Part 2
+                    result = GetSumOfSmallestAndLargest(lines[i], lines.Take(i).ToArray());
+                    //End Part 2
                     break;
                 }
             }
