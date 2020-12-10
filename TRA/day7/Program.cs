aca193c951308674ab71ec55cb5c50cf03f9d4ee
ny toky <22963493+nytokyr@users.noreply.github.com>
2020-12-10 19:28:41 +0100

    add to master

---
+++ TRA/day7/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace day7
+{
+    class Program
+    {
+        #region part1
+        static Dictionary<string, Dictionary<string, int>> dico = new Dictionary<string, Dictionary<string, int>>();
+        #endregion
+        static HashSet<string> h = new HashSet<string>();
+        static int sommetotale = 0;
+        static void Main(string[] args)
+        {
+            var input = File.ReadAllLines("input.txt");
+            #region part1
+            foreach (var item in input)
+            {
+                Tokenizer(item);
+            }
+
+            recursive("shiny gold bag", 1);//1  bag
+
+            //foreach (var hh in h)
+            //{
+            //    Console.WriteLine(hh);
+            //}
+
+            Console.WriteLine($"somme = {sommetotale}");
+            #endregion
+        }
+
+        private static void recursive(string input, int previousValue)
+        {
+            #region part1
+            input = input.Trim();
+            //// var res = dico.Where(x => x.Value.Any(_ => _.Contains(input.LastIndexOf('s') == input.Length - 1 ? input.Substring(0, input.LastIndexOf('s') - 1) : input))).Select(k => k.Key);
+            // foreach (var kp in res)
+            // {
+
+            //     recursive(kp);
+            //     if (!h.Contains(kp))
+            //         h.Add(kp);
+            // }
+            #endregion
+
+            //s += dico[input].Select(x => x.Value).Sum();
+            var localcount = 0;
+            foreach (var item in dico[input])
+            {
+
+                localcount += item.Value;
+                Console.WriteLine($" parent = {input} => {item.Value} {item.Key} =>  NextrecursiveParam = {previousValue * item.Value}");
+                recursive(item.Key, item.Value * previousValue);
+            }
+            sommetotale += (localcount * previousValue);
+            Console.WriteLine($"sommetotale+= {localcount} * {previousValue} => sommetotale = {sommetotale}");
+
+        }
+
+        static void Tokenizer(string input)
+        {
+            #region part1
+            var tokens = input.Split("contain");
+            var t2 = tokens[1].Split(",");
+
+            var r = new Regex(@"((?<count>[0-9])(?<grouped>[a-z ]+bag))");
+            var matched = r.Matches(tokens[1]);
+            // if (dico.ContainsKey(tokens[0]))
+            string key = tokens[0].Replace(" bags", " bag").Trim();
+            dico.Add(key, new Dictionary<string, int>());
+            foreach (Match _ in matched)
+            {
+                dico[key].Add(_.Groups["grouped"].Value, int.Parse(_.Groups["count"].Value));
+            }
+            #endregion
+        }
+
+
+
+
+    }
+}
---
+++ TRA/day8/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace day8
+{
+    class Program
+    {
+         
+        static int accumulator = 0;
+        static void Main(string[] args)
+        {
+            var input = File.ReadAllLines("input.txt");
+            //int i = 0;
+            //int previ = 0;
+            //while (i < input.Length)
+            //{
+            //    var splitted = input[i].Split(" ");
+            //    var instr = splitted[0];
+            //    if (dico.ContainsKey(i))
+            //    {
+            //        i = previ+1;
+            //        continue;
+            //    }
+            //    dico.Add(i, true);
+            //    if (instr == "acc")
+            //    {
+            //        accumulator += int.Parse(splitted[1]);
+            //    }
+            //    else
+            //    if ("jmp" == instr)
+            //    {
+            //        previ = i;
+            //        i += int.Parse(splitted[1]);
+            //        continue;
+            //    }
+            //    i++;
+            //}
+            //Console.WriteLine(accumulator);
+
+            //var input = File.ReadAllLines("input.txt");
+            var eeee = input.Select((s, i) => new { i, s }).Where(_ => _.s.Contains("jmp"));
+            string[] oooo = new string[input.Length];
+            Array.Copy(input, oooo, input.Length);
+            foreach (var ii in eeee)
+            {
+                if (!runprogram(oooo))
+                {
+                    Array.Copy(input, oooo, input.Length);
+                    oooo[ii.i] = oooo[ii.i].Replace("jmp", "nop");
+                    if (runprogram(oooo))
+                        break;
+                }
+                else
+                    break;
+               
+            }
+            Console.WriteLine(accumulator);
+        }
+
+        static bool runprogram(string[] files)
+        {
+            int i = 0;
+            var dico = new Dictionary<int, bool>();
+            while (i < files.Length)
+            {
+                var splitted = files[i].Split(" ");
+                var instr = splitted[0];
+
+                if (dico.ContainsKey(i))
+                {
+                    accumulator = 0;
+                    return false;
+                }
+                    dico.Add(i, true);
+             
+                if (instr == "acc")
+                {
+                    accumulator += int.Parse(splitted[1]);
+                }
+                else
+                      if ("jmp" == instr)
+                {
+                    i += int.Parse(splitted[1]);
+                    continue;
+                }
+                i++;
+            }
+            return true;
+        }
+
+    }
+
+
+
+
+}
